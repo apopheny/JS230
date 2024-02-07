96f1d2278ae6d2791f8b8bf3c2df0501aca2166b
apopheny <cedouglass@gmail.com>
2024-02-07 17:54:04 -0500

    wordguess base logic and event listeners added

--- guess-a-word/javascripts/game.js
+++ guess-a-word/javascripts/game.js
@@ -1,14 +1,119 @@
-const WORD_BANK = ["apple", "banana", "orange", "pear"];
+(function () {
+  const WORD_BANK = ["apple", "banana", "orange", "pear"];
+  class GuessWord {
+    constructor() {
+      this.unusedWords = [...WORD_BANK];
+      this.chosenWord = null;
+      this.triesRemaining = null;
+      this.previousGuesses = [];
 
-class GuessWord {
-  constructor() {
-    this.unusedWords = [...WORD_BANK];
-    this.usedWords = [];
+      this.apples = document.querySelector("#apples");
+      this.message = document.querySelector("#message");
+      this.spaces = document.querySelector("#spaces");
+      this.guesses = document.querySelector("#guesses");
+      this.newGameLink = document.querySelector("#replay");
+      this.newGameLink.hidden = true;
+
+      document.addEventListener("DOMContentLoaded", this.bindEventListeners);
+    }
+
+    bindEventListeners(_) {
+      function userGuess(event) {
+        event.preventDefault();
+        game.userGuess(event.key);
+      }
+
+      document.addEventListener("keyup", game.userGuess);
+      game.newGameLink.addEventListener("click", game.newGame);
+      game.newGameLink.dispatchEvent(new MouseEvent("click"));
+    }
+
+    randomWord() {
+      let wordIndex = Math.floor(Math.random() * this.unusedWords.length);
+      return this.unusedWords.splice(wordIndex, 1)[0];
+    }
+
+    newGame(event) {
+      event.preventDefault();
+      game.chosenWord = game.randomWord();
+      console.log(game.chosenWord);
+      if (game.chosenWord === undefined) {
+        game.clearLetters();
+        game.wordsExhausted();
+      } else {
+        game.clearLetters();
+        game.generateSpaces();
+        game.newGameLink.hidden = true;
+        game.triesRemaining = 6;
+        game.previousGuesses = [];
+      }
+    }
+
+    clearLetters() {
+      [...this.spaces.children, ...this.guesses.children].forEach((child) => {
+        if (child.tagName === "SPAN") child.remove();
+      });
+    }
+
+    generateSpaces() {
+      for (let i = 0; i < this.chosenWord.length; i++) {
+        let space = document.createElement("span");
+        this.spaces.appendChild(space);
+      }
+    }
+
+    wordsExhausted() {
+      this.message.textContent = "Sorry, I've run out of words!";
+    }
+
+    userGuess(event) {
+      event.preventDefault();
+      let guessAttempt = event.key.toLowerCase();
+
+      if (guessAttempt.length > 1) return;
+      if (game.previousGuesses.includes(guessAttempt)) return;
+      game.previousGuesses.push(guessAttempt);
+
+      if (game.chosenWord.match(guessAttempt)) {
+        game.letterMatched(guessAttempt);
+      } else {
+        game.letterNotMatched(guessAttempt);
+      }
+
+      console.log(game.triesRemaining);
+    }
+
+    letterMatched(guessAttempt) {
+      [...this.chosenWord].forEach((letter, index) => {
+        if (letter === guessAttempt) {
+          this.spaces.children[index + 1].textContent = letter.toUpperCase();
+        }
+      });
+      console.log([...this.spaces.children].map((child) => child.innerText));
+      if (
+        [...this.spaces.children].every((span) => span.innerText.length > 0)
+      ) {
+        this.userWin();
+      }
+    }
+
+    letterNotMatched(guessAttempt) {
+      let span = document.createElement("span");
+      span.textContent = guessAttempt.toUpperCase();
+      this.guesses.appendChild(span);
+      this.triesRemaining -= 1;
+
+      if (this.triesRemaining < 1) this.userLose();
+    }
+
+    userWin() {
+      console.log("You are fired!");
+    }
+
+    userLose() {
+      console.log("I have a bit of bad news...");
+    }
   }
 
-  randomWord() {
-    let wordIndex = Math.floor(Math.random() * this.unusedWords.length);
-    this.usedWords.push(this.unusedWords[wordIndex]);
-    return this.unusedWords.splice(wordIndex, 1);
-  }
-}
+  const game = new GuessWord();
+})();
