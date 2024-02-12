83ee20801f828c511706243cd16e72def4462889
apopheny <cedouglass@gmail.com>
2024-02-12 08:04:43 -0500

    schedule updating in need of significant refactor for templating logic between multiple schedules

--- booking_app_node/public/javascripts/addSchedule.js
+++ booking_app_node/public/javascripts/addSchedule.js
@@ -1,7 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   Handlebars.registerPartial(
     "staffDropdownPartial",
     document.querySelector("#staffDropdownPartial").innerHTML
+  );
+
+  Handlebars.registerPartial(
+    "staffDatesPartial",
+    document.querySelector("#staffDropdownPartial").innerHTML
+  );
+
+  Handlebars.registerPartial(
+    "staffTimePartial",
+    document.querySelector("#staffTimePartial").innerHTML
   );
 
   let formTemplate = Handlebars.compile(
@@ -9,6 +19,9 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   const main = document.querySelector("#main");
+  const submit = document.querySelector("#scheduleSubmit");
+  const newSchedule = document.querySelector("#newSchedule");
+
   async function retrieveStaff() {
     try {
       let request = await fetch("/api/staff_members", {
@@ -28,9 +41,61 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  retrieveStaff()
-    .then((data) => {
-      main.insertAdjacentHTML("beforeend", formTemplate({ data }));
-    })
-    .catch(console.error);
+  async function retrieveDates(staff_id) {
+    try {
+      let request = await fetch(`/api/schedules/#{staff_id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      if (!request.ok)
+        throw new Error(`Staff retrieval failed. Status: ${request.status}`);
+
+      let data = await request.json();
+      data = data.filter((slot) => slot.student_email === null);
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  retrieveDates();
+
+  let dropdownRenderer;
+  async function makeNamesDropdownRenderer(count = 0) {
+    if (!dropdownRenderer) {
+      try {
+        const staff = await retrieveStaff();
+        console.log(staff);
+        let staffRosterHTML = formTemplate({ data: staff });
+
+        dropdownRenderer = function () {
+          main.insertAdjacentHTML("beforeend", staffRosterHTML);
+        };
+      } catch (error) {
+        if (count >= 3) throw error;
+
+        count += 1;
+        return makeNamesDropdownRenderer(count);
+      }
+    }
+
+    return dropdownRenderer;
+  }
+
+  dropdownRenderer = await makeNamesDropdownRenderer()
+    .then((renderer) => renderer())
+    .catch((error) =>
+      console.error(
+        `Attempt to fetch staff roster failed after multiple attempts. Reason: ${error.message}`
+      )
+    );
+
+  newSchedule.addEventListener("click", await makeNamesDropdownRenderer());
+
+  document.body.insertAdjacentElement("beforeend", submit);
 });
