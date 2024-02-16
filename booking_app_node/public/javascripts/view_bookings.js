6e26932aed596219dede652950e72cf60530d189
apopheny <cedouglass@gmail.com>
2024-02-16 17:04:39 -0500

    view bookings exercise complete

--- booking_app_node/public/javascripts/view_bookings.js
+++ booking_app_node/public/javascripts/view_bookings.js
@@ -2,20 +2,61 @@ document.addEventListener("DOMContentLoaded", async () => {
   class Booking {
     #dates;
     #templates;
+
     constructor() {
       this.#dates = null;
       this.#templates = {};
+      this.bookingDates = null;
     }
 
     init() {
       this.getTemplates();
-      this.renderBookings();
+      this.renderBookings().then(() => this.bindEventListeners());
     }
 
     getTemplates() {
       this.#templates.bookingDatesTemplate = Handlebars.compile(
-        document.getElementById("datesTemplate").innerHTML
+        $("#datesTemplate").html()
       );
+
+      this.#templates.bookingDetailTemplate = Handlebars.compile(
+        $("#bookingDetails").html()
+      );
+    }
+
+    bindEventListeners() {
+      function dateClickEvent(event) {
+        let date = $(event.target).attr("data-date");
+
+        this.renderDetails(date);
+      }
+
+      $("#booking_dates").on("click", dateClickEvent.bind(this));
+    }
+
+    renderDetails(date) {
+      this.getDetails(date)
+        .then((res) => this.parseDetails(res))
+        .then((res) => {
+          let $ele = $(`[data-date=${date}]`);
+          if ($ele.children().length > 0) {
+            $ele
+              .children()
+              .replaceWith(this.#templates.bookingDetailTemplate(res));
+          } else {
+            $ele.append(this.#templates.bookingDetailTemplate(res));
+          }
+        })
+        .catch(console.error);
+    }
+
+    parseDetails(infoArray) {
+      let result = infoArray.map((details) => {
+        let [staff, studentEmail, time] = details;
+        return { staff, studentEmail, time };
+      });
+
+      return result;
     }
 
     getDates() {
@@ -26,10 +67,17 @@ document.addEventListener("DOMContentLoaded", async () => {
           "Content-Type",
           "application/json; charset=utf-8"
         );
+
         request.addEventListener("load", () => {
           if (request.status < 200 || request.status >= 300) {
             reject(
               `Failed to load bookings. ${request.status}: ${request.statusText}`
+            );
+          }
+
+          if (request.error) {
+            reject(
+              `Error encountered during bookings request. ${request.status}`
             );
           }
 
@@ -42,13 +90,48 @@ document.addEventListener("DOMContentLoaded", async () => {
       return requestPromise;
     }
 
+    getDetails(date) {
+      let requestPromise = new Promise((resolve, reject) => {
+        let request = new XMLHttpRequest();
+        request.open("GET", `/api/bookings/${date}`);
+        request.setRequestHeader(
+          "Content-Type",
+          "application/json; charset=utf-8"
+        );
+
+        request.onload = () => {
+          if (request.status < 200 || request.status >= 300) {
+            reject(
+              `Failed to load booking details. ${request.status}: ${request.statusText}`
+            );
+          }
+
+          this.bookingDates = JSON.parse(request.response);
+          resolve(this.bookingDates);
+        };
+
+        request.onerror = () => {
+          reject(`Booking details request failed due to ${request.status}`);
+        };
+
+        request.send();
+      });
+
+      return requestPromise;
+    }
+
     renderBookings() {
       let $main = $("main");
-      this.getDates()
-        .then((res) => this.#templates.bookingDatesTemplate(res))
-        .then((res) => $main.append(res));
+
+      return this.getDates()
+        .then((res) => {
+          return this.#templates.bookingDatesTemplate(res);
+        })
+        .then((res) => $main.append(res))
+        .catch(console.error);
     }
   }
 
-  let app = new Booking().init();
+  let app = new Booking();
+  app.init();
 });
